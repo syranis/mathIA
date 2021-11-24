511613320b7cd09424b96098d854c2948f542ff9
syranis <elliott.kalt@gmail.com>
2021-11-24 15:34:14 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import math
+import requests
+
+# api for https://www.wunderground.com/history/monthly/KLGA/date/2020-10
+url = 'https://api.weather.com/v1/location/KLGA:9:US/observations/historical.json?apiKey=' \
+      'e1f10a1e78da46f5b10a1e78da96f525&units=e&startDate=20201001&endDate=20201031'
+
+r = requests.get(url=url).json()  # get raw data from api; this can be found in addendum 1
+
+temps = []
+for datapoint in r['observations']:
+    # the first data point from each datapoint has the max temperature from that day
+    # this checks that there is a value for max_temp and adds it to a list
+    if datapoint['max_temp'] is not None:
+        temps.append(int(datapoint['max_temp']))
+
+
+# here the max temperature from each day is converted into a state, where 0 is 45-49 fahrenheit, 1 is 50-54, etc.
+state = []
+for i in range(len(temps)):
+    state.append(math.floor(temps[i] / 5) - 9)
+
+for i in range(len(state)):
+    print(f'{i} | {state[i]}')
+
