d5434f881e862fc48524595ea6dc54c5e1e68cc2
ShoddyMaintenance <elliott.kalt@gmail.org>
2021-11-29 11:45:28 -0500

    more verbose comments

--- main.py
+++ main.py
@@ -3,34 +3,31 @@ import pandas as pd
 import requests
 import matplotlib.pyplot as plt
 
-# api endpoint for https://www.wunderground.com/history/monthly/KLGA/date/2020-10
-# this has 923 weather data points from LaGuardia airport in NYC over the month of October 2020 under JSON format
+# Data from the api endpoint of https://www.wunderground.com/history/monthly/KLGA/date/2020-10 is being used
+# The data has 923 weather data points from LaGuardia airport in NYC over the month of October 2020 under JSON format
 url = 'https://api.weather.com/v1/location/KLGA:9:US/observations/historical.json?apiKey=' \
       'e1f10a1e78da46f5b10a1e78da96f525&units=e&startDate=20201001&endDate=20201031'
+r = requests.get(url=url).json()
 
-r = requests.get(url=url).json()  # get raw data from api; this can be found in addendum 1
-
+# The first data point from each day has the max temperature from that day.
+# This checks that there is a value for max_temp and adds it to a list if it is there.
 temps = []
 for datapoint in r['observations']:
-    # the first data point from each day has the max temperature from that day
-    # this checks that there is a value for max_temp and adds it to a list if it is there
     if datapoint['max_temp'] is not None:
         temps.append(int(datapoint['max_temp']))
 
-# here the max temperature from each day is converted into a state, where 0 is 55-59, 1 is 60-64, etc.
-# this is done by dividing by five and rounding down then subtracting 11, giving 5 values from 0 to 4
+# The following converts the max temperature from each day into a state, where 0 is 55-59, 1 is 60-64, etc.
+# This is done by dividing each value by five and both rounding down and subtracting 11, giving 5 values from 0 to 4
 states = []
 for i in range(len(temps) - 1):
     states.append(temps[i] // 5 - 11)
 
-# create a 5x5 matrix
+# The following determines the probabilities of going from one state to the next.
+# This is done by iterating over each combination of starting state and ending state to calculate the percentage of
+# times that the starting state leads to the ending state and updating the corresponding value in the transitionMatrix
+# array.
 matrixSize = 5
 transitionMatrix = np.array(np.arange(matrixSize ** 2), dtype=float).reshape(matrixSize, matrixSize)
-
-# determine the probabilities of going from one state to the next:
-# for each combination of starting state and ending state,
-# calculate the percentage of times that the starting state leads to the ending state
-# update the corresponding value in the transitionMatrix array
 for stateStart in range(matrixSize):
     for stateEnd in range(matrixSize):
         start = 0
@@ -49,15 +46,21 @@ print(f'The transition matrix is:\n{transitionMatrix}')
 # This works because the stationary distribution is a vector "stationaryDistribution" which when multiplied with the
 # transition matrix returns a vector which can be iteratively multiplied against the transition matrix to reach an
 # equilibrium state.
+# Once two consecutive stationary distributions are equal, the stationary distribution has been calculated.
 stationaryDistribution = np.array([[1.0, 0.0, 0.0, 0.0, 0.0]])
 stateHist = stationaryDistribution
 dfStateHist = pd.DataFrame(stationaryDistribution)
 distr_hist = [[0, 0, 0]]
-for x in range(50):
+count = 0
+while True:
+    if (stationaryDistribution == np.dot(stationaryDistribution, transitionMatrix)).all():
+        print(f'Stationary distribution reached at {count} iterations:\n{stationaryDistribution}')
+        break
+    count += 1
     stationaryDistribution = np.dot(stationaryDistribution, transitionMatrix)
-    if x == 49:
-        print(f'The stationary distribution is:\n{stationaryDistribution}')
     stateHist = np.append(stateHist, stationaryDistribution, axis=0)
     dfDistrHist = pd.DataFrame(stateHist)
     dfDistrHist.plot()
 plt.show()
+
+# The following simulates the markov chain.
