6fd8e695b4f816c543ec31e3b7aa49cc8fc8464c
ShoddyMaintenance <elliott.kalt@gmail.org>
2021-11-29 11:17:17 -0500

    stationary distribution implemented

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
+import pandas as pd
 import requests
+import matplotlib.pyplot as plt
 
 # api endpoint for https://www.wunderground.com/history/monthly/KLGA/date/2020-10
 # this has 923 weather data points from LaGuardia airport in NYC over the month of October 2020 under JSON format
@@ -23,12 +25,12 @@ for i in range(len(temps) - 1):
 
 # create a 5x5 matrix
 matrixSize = 5
-transitionStates = np.array(np.arange(matrixSize ** 2), dtype=float).reshape(matrixSize, matrixSize)
+transitionMatrix = np.array(np.arange(matrixSize ** 2), dtype=float).reshape(matrixSize, matrixSize)
 
 # determine the probabilities of going from one state to the next:
 # for each combination of starting state and ending state,
 # calculate the percentage of times that the starting state leads to the ending state
-# update the corresponding value in the transitionStates array
+# update the corresponding value in the transitionMatrix array
 for stateStart in range(matrixSize):
     for stateEnd in range(matrixSize):
         start = 0
@@ -38,6 +40,24 @@ for stateStart in range(matrixSize):
                 start += 1
                 if states[i + 1] == stateEnd:
                     end += 1
-        transitionStates[stateStart, stateEnd] = end / start
+        transitionMatrix[stateStart, stateEnd] = end / start
 
-print(transitionStates)
+print(f'The transition matrix is:\n{transitionMatrix}')
+
+# The following finds the stationary distribution of the markov chain.
+# The stationary distribution is found here by raising the transition matrix to the power of number of iterations.
+# This works because the stationary distribution is a vector "stationaryDistribution" which when multiplied with the
+# transition matrix returns a vector which can be iteratively multiplied against the transition matrix to reach an
+# equilibrium state.
+stationaryDistribution = np.array([[1.0, 0.0, 0.0, 0.0, 0.0]])
+stateHist = stationaryDistribution
+dfStateHist = pd.DataFrame(stationaryDistribution)
+distr_hist = [[0, 0, 0]]
+for x in range(50):
+    stationaryDistribution = np.dot(stationaryDistribution, transitionMatrix)
+    if x == 49:
+        print(f'The stationary distribution is:\n{stationaryDistribution}')
+    stateHist = np.append(stateHist, stationaryDistribution, axis=0)
+    dfDistrHist = pd.DataFrame(stateHist)
+    dfDistrHist.plot()
+plt.show()
